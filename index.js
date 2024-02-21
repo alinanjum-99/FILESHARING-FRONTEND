7e183332eac325d977ddcd9ff8d1b2282736f3bb
alinanjum-99 <alinanjum9@gmail.com>
2024-02-21 17:31:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://alinfilesharing.onrender.com";
+const baseURL = "https://filesharing-backend-jci4.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
